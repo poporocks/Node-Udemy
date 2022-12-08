2908b3a9a964b3dc1e430f969c8653b4c63a9411
Edgar <edgar.lao89@gmail.com>
2022-12-07 17:51:44 -0700

    Inicia Ejercicio 07-RestServer 20221207

---
+++ 07-restserver/models/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        //Middleware
+        this.middleware();
+
+        //Rutas de mi aplicación
+        this.routes();
+    }
+
+    middleware() {
+        //Directorio Público
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.get('/api', (req, res) => {
+            res.json({
+                msg: 'get API'
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                msg: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'delete API'
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'patch API'
+            });
+        });
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Servidor corriendo en puerto: ", this.port);
+        });
+    }
+}
+
+module.exports = Server;
